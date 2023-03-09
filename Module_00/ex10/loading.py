d99de1cc0f2a2c4413f0b84d776b9132728a0899
Mohamed fadel Labrayji <mlabrayj@e2r3p3.1337.ma>
2023-03-09 21:38:33 +0100

    editex10

--- Module_00/ex10/loading.py
+++ Module_00/ex10/loading.py
@@ -5,15 +5,18 @@ def ft_progress(listy):
     for i in range(len(listy)):
         percent = i / len(listy) * 100
         print(f"\rETA: {int((len(listy) - i) * 0.01)}s "           # Estimated time remaining in seconds
-      f"[{int(percent)}%] "                                 # Percentage of completion
-      f"[{'=' * int(percent / 5)}{'>' if percent % 5 != 0 else ''}{' ' * (20 - int(percent / 5))}] "   # Visual progress bar
-      f"{i}/{len(listy)} | "                                 # Progress counter
-      f"elapsed time {int(i * 0.01)}s", end="")              # Time elapsed in seconds
+              # Percentage of completion
+              f"[{int(percent)}%] "
+              # Visual progress bar
+              f"[{'=' * int(percent / 5)}{'>' if percent % 5 != 0 else ''}{' ' * (20 - int(percent / 5))}] "
+              # Progress counter
+              f"{i}/{len(listy)} | "
+              f"elapsed time {int(i * 0.01)}s", end="")              # Time elapsed in seconds
 
         yield listy[i]
 
 
-listy = range(1000)
+listy = range(8000)
 ret = 0
 for elem in ft_progress(listy):
     ret += (elem + 3) % 5
