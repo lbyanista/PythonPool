6e38fb7b14213d0d63c02de0605adc0fd17d2e83
lbyanista <fadali_1992@hotmail.com>
2023-03-09 16:27:57 +0100

    edit ex10

--- Module_00/ex10/loading.py
+++ Module_00/ex10/loading.py
@@ -4,7 +4,12 @@ import time
 def ft_progress(listy):
     for i in range(len(listy)):
         percent = i / len(listy) * 100
-        print(f"\rETA: {int((len(listy) - i) * 0.01)}s [{int(percent)}%] [{'=' * int(percent / 5)}{'>' if percent % 5 != 0 else ''}{' ' * (20 - int(percent / 5))}] {i}/{len(listy)} | elapsed time {int(i * 0.01)}s", end="")
+        print(f"\rETA: {int((len(listy) - i) * 0.01)}s "           # Estimated time remaining in seconds
+      f"[{int(percent)}%] "                                 # Percentage of completion
+      f"[{'=' * int(percent / 5)}{'>' if percent % 5 != 0 else ''}{' ' * (20 - int(percent / 5))}] "   # Visual progress bar
+      f"{i}/{len(listy)} | "                                 # Progress counter
+      f"elapsed time {int(i * 0.01)}s", end="")              # Time elapsed in seconds
+
         yield listy[i]
 
 
